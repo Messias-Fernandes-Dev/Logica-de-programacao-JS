4e3130ee849180a7e5210c78813f6cf02dacdcd1
Messias Fernandes <dev.messiasfer@gmail.com>
2026-08-12 10:13:04 -0300

    supermercado JS

---
+++ cap02/exercicios/supermercadoJS/super.js
@@ -0,0 +1,16 @@
+const frm = document.querySelector("form")
+const resp1 = document.querySelector("#outResp1")
+const resp2 = document.querySelector("#outResp2")
+
+frm.addEventListener("submit", (e) => {
+  const produto = frm.inProduto.value
+  const preco = Number(frm.inPreco.value)
+
+  const desconto = preco / 2 
+  const valorTotal = (preco * 3) - desconto
+
+  resp1.innerText = `${produto} - Promoção: Leve 3 por R$: ${valorTotal.toFixed(2)}`
+  resp2.innerText = `O 3° produto custa apenas R$: ${desconto.toFixed(2)}`
+
+  e.preventDefault()
+})
