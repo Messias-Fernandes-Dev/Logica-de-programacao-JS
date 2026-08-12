03a62702f1322975e5c96ceacd72b959bc466f5d
Messias Fernandes <dev.messiasfer@gmail.com>
2026-08-12 09:31:54 -0300

    Exercícios farmáciaJS / restauranteJS / revendaJS

---
+++ cap02/exercicios/farmaciaJS/farmacia.js
@@ -0,0 +1,16 @@
+const frm = document.querySelector("form")
+const resp1 = document.querySelector("#outResp1")
+const resp2 = document.querySelector("#outResp2")
+
+frm.addEventListener("submit", (e) => {
+  const medicamento = frm.inMedicamento.value
+  const preco = Number(frm.inPreco.value)
+
+  const valor = Math.floor(preco)
+  const promocao = valor * 2
+
+  resp1.innerText = `Promoção de ${medicamento}`
+  resp2.innerText = `Leve 2 por apenas R$: ${promocao}`
+
+  e.preventDefault()
+})
--- cap02/exercicios/filmesJS/filmes.js
+++ cap02/exercicios/filmesJS/filmes.js
@@ -1,16 +1,18 @@
+// cria referência ao form e aos elementos h3 e h4
 const frm = document.querySelector("form")
 const resp1 = document.querySelector("h3")
 const resp2 = document.querySelector("h4")
 
+// Cria um "ouvinte" de envento, acianado quando o botão submit for clicado
 frm.addEventListener("submit", (e) => {
-  const titulo = frm.inTitulo.value 
+  const titulo = frm.inTitulo.value  // obtém os conteúdos dos campos 
   const durucao = Number(frm.inDuracao.value)
 
-  const horas = Math.floor(durucao / 60)
-  const minutos = durucao % 60
+  const horas = Math.floor(durucao / 60) // arredonda para baixo os resultados
+  const minutos = durucao % 60 // obtém o resto da divisão 
 
-  resp1.innerText = titulo
+  resp1.innerText = titulo // exibe as respostas 
   resp2.innerText = `${horas} horas(s) e ${minutos} minuto(s)`
 
-  e.preventDefault()
+  e.preventDefault() // evita o envio do form = reload da pág 
 })
